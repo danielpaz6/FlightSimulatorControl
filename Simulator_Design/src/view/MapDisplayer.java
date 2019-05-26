1cc0aa584cddafe7fc96a568f7c740a349ac5b4d
omer nahum <omernah93@gmail.com>
2019-05-26 15:56:48 +0300

    made new component

---
+++ Simulator_Design/src/view/MapDisplayer.java
@@ -0,0 +1,33 @@
+package view;
+
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+public class MapDisplayer extends Canvas {
+	
+	double[][] coordinates;
+	
+	public MapDisplayer(){
+		
+	}
+	
+	public void redraw() {
+		
+		double width = (double)(this.getWidth()) ;
+		double height = (double)(this.getHeight()) ;
+		double widthBlock = width / coordinates[0].length;
+		double heightBlock = height / coordinates.length;
+		
+		GraphicsContext gc = getGraphicsContext2D();
+		
+		for(int i = 0; i < coordinates.length;i++) {
+			for(int j = 0; j<coordinates[0].length; j++) {
+				gc.fillRect(i*widthBlock, j*heightBlock, widthBlock,heightBlock);
+				gc.setFill(new Color(0.0,0.0,255.0,1.0));
+			}
+		}
+	}
+}
